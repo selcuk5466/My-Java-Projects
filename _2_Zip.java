2e7bfae8680b1ab101c03d136a4da486e2055fa6
selcuk5466 <130847870+selcuk5466@users.noreply.github.com>
2024-08-19 22:21:10 +0300

    Add files via upload

---
+++ _2_Zip.java
@@ -0,0 +1,33 @@
+package task2;
+
+import java.util.Scanner;
+
+public class _2_Zip {
+    public static void main(String[] args) {
+        // String str = "aaabbccccddd";
+     compressText();
+    }
+        public static void compressText(){
+        Scanner oku=new Scanner(System.in);
+        System.out.print("Bir metin giriniz: ");  String str=oku.nextLine();
+        //String str="Buuuugüüünnnnn ggggüüüünllerden Cuuummmaaarrteeeesiiiii";
+        char[] charArray = str.toCharArray();
+        for ( int i =1 ; i < charArray.length; i++)  //bir for döngüsü kuruyoruz.
+        {         int sayac=1;
+
+                while ((charArray[i-1] == charArray[i])) {
+                      sayac = sayac + 1;
+                      i++;
+                     if (i==charArray.length)
+                            break;
+                }
+            System.out.print(sayac + "" + charArray[i-1]);
+
+        }
+    }
+}
+
+
+
+
+
